2e0350d4dacd629519e058f20a71687ea4cc4800
Mahmoud Fares <knight6700@gmail.com>
2021-11-01 12:11:11 +0200

    Finish Apps Screen

---
+++ AppStore/Helper/baseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  baseViewController.swift
+//  AppStore
+//
+//  Created by Mahmoud Fares on 01/11/2021.
+//
+
+import UIKit
+class BaseListController: UICollectionViewController {
+    
+    init() {
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
